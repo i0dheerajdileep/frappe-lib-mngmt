ff7de94be10634ce7691609b4708ec893da9aa9a
Dheeraj <i.dheerajdileep@gmail.com>
2024-01-07 23:58:56 +0530

    library membership cehck

--- library_management/library_management/doctype/library_membership/library_membership.py
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -1,9 +1,20 @@
-# Copyright (c) 2024, dheeraj and contributors
-# For license information, please see license.txt
+from __future__ import unicode_literals
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class LibraryMembership(Document):
-	pass
+    # check before submitting this document
+    def before_submit(self):
+        exists = frappe.db.exists(
+            'Library Membership',
+            {
+                'library_member': self.library_member,
+                # check for submitted documents
+                'docstatus': 1,
+                # check if the membership's end date is later than this membership's start date
+                'to_date': ('>', self.from_date),
+            },
+        )
+        if exists:
+            frappe.throw('There is an active membership for this member')
